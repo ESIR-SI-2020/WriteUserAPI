b0d88c8aa31bf5841185c3753531cdf377c2c7eb
Roland <rolandkouassi1@hotmail.fr>
2019-10-07 12:18:36 +0200

    Hello world

---
+++ src/main/java/models/User.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.util.List;
+
+public class User {
+    String username;
+    String email;
+    String photoUrl;
+    String bio;
+    Address address;
+    List<String> friendsId;
+}
